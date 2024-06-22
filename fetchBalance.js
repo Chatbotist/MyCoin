92e81c9961e315e7b50c16033b291d765e5d8b1e
Дмитрий <143331027+Chatbotist@users.noreply.github.com>
2024-06-22 18:29:13 +0300

    Update fetchBalance.js

--- fetchBalance.js
+++ fetchBalance.js
@@ -44,11 +44,3 @@ export default async function handler(req, res) {
     res.status(405).json({ error: 'Метод не поддерживается' });
   }
 }
-
-// Добавляем поддержку CORS
-export const config = {
-  api: {
-    bodyParser: true,
-    externalResolver: true,
-  },
-};
